86423e85e2dda0b4bf0fc3dc8f2f23f6923a4ad1
RuslanMonar <ruslanmonar@gmail.com>
2023-05-03 19:30:42 +0300

    reset red timers

--- src/app/components/Tasks/CreateTaskSidebar.jsx
+++ src/app/components/Tasks/CreateTaskSidebar.jsx
@@ -24,6 +24,7 @@ export const CreateTaskSidebar = ({ isInputActive }) => {
         setSelectedStartDate(null);
         setSelectedPriority("gray");
         setSelectedProject(null);
+        setClickedIcons([]);
     }, [isInputActive]);
 
     const getPriority = () => {
